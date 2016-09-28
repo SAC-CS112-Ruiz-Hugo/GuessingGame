22c8f84214feadb4473729344dbeaea219bc33b5
SAC-CS112-Ruiz-Hugo <HjRuiz93@yahoo.com>
2016-09-27 23:49:36 -0700

    Add files via upload

---
+++ Guessing_Game.java
@@ -0,0 +1,94 @@
+/**
+ * Created by Culebra on 9/25/2016.
+ */
+    // This is a guess a number program
+    // It needs to be modified to ask after each round if they want to keep playing
+
+    import java.util.Random;
+    import java.util.Scanner;
+
+    public class Guessing_Game {
+        public static void main(String[] args) {
+            Random rand = new Random();
+            int numberToGuess = rand.nextInt(20);
+            int numberOfGamesPlayed = 0;
+            Scanner input = new Scanner(System.in);
+            int guess;
+            int numberOfBadGuesses = 0;
+            int numberOfGoodGuesses = 0;
+            float number;
+
+            boolean win = false;
+            char answer;
+
+            // Start of first while loop
+            while (win == false) {
+
+                System.out.println("Guess a number between 1 and 20: ");
+                guess = input.nextInt();
+
+                if (guess == numberToGuess) {
+                    win = true;
+                    numberOfGoodGuesses++;
+                } else if (guess < numberToGuess) {
+                    System.out.println("Your guess is too low");
+                    numberOfBadGuesses++;
+                } else if (guess > numberToGuess) {
+                    System.out.println("Your guess is too high");
+                    numberOfBadGuesses++;
+                }
+            }
+            System.out.println("You win!");
+            numberOfGamesPlayed++;
+            System.out.println("Would you like to keep playing? (Y/N)");
+            answer = input.next().charAt(0);
+
+            if (answer == 'Y') {
+                // Start of second while loop
+                while (answer == 'Y') {
+                    System.out.println("Guess a number between 1 and 20: ");
+                    guess = input.nextInt();
+
+                    if (guess == numberToGuess) {
+                        win = true;
+                        System.out.println("You win!");
+                        numberOfGamesPlayed++;
+                        numberOfGoodGuesses++;
+                        System.out.println("Would you like to keep playing? (Y/N)");
+                        answer = input.next().charAt(0);
+                    }
+                    else if (guess < numberToGuess) {
+                        System.out.println("Your guess is too low");
+                        numberOfBadGuesses++;
+                    }
+                    else if (guess > numberToGuess) {
+                        System.out.println("Your guess is too high");
+                        numberOfBadGuesses++;
+                    }
+
+                }
+            }
+            else {
+                number = (numberOfBadGuesses + numberOfGoodGuesses);
+                number = (number * 100 / numberOfGamesPlayed);
+                // ======= OUTPUT ======
+
+                System.out.println("The number was " + numberToGuess);
+                System.out.println("Number of games you played was " + numberOfGamesPlayed);
+                System.out.println("You guess bad " + numberOfBadGuesses + " times.");
+                System.out.println("You guess right " + numberOfGoodGuesses + " times!");
+                System.out.println("Overall guess percentage is " + number);
+                System.exit(0);
+            }
+
+            number = (numberOfBadGuesses + numberOfGoodGuesses);
+            number = (number * 100 / numberOfGamesPlayed);
+
+            // ======= OUTPUT ======
+            System.out.println("The number was " + numberToGuess);
+            System.out.println("Number of games you played was " + numberOfGamesPlayed);
+            System.out.println("You guess bad " + numberOfBadGuesses + " times.");
+            System.out.println("You guess right " + numberOfGoodGuesses + " times!");
+            System.out.println("Overall guess percentage is " + number);
+        }   // End of main method
+    }   // End of Guessing_Game
